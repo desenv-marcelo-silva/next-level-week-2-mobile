6c5e714d44714438cb3a3f6eff1b7b6bd4378e0d
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-08-09 12:01:36 -0300

    style(pageHeader): more padding bottom

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
-    paddingBottom: 10,
+    paddingBottom: 30,
     paddingHorizontal: 40,
     backgroundColor: '#8257e5',
   },
